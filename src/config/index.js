ce2cb34841604cb4bbb4b7cae3cfacf8fa93d4c8
KostyaKorshunov <korshunovk@gmail.com>
2022-09-05 16:58:45 +0300

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,3 @@
-const dotenv = require('dotenv');
-const path = require('path');
-
-dotenv.config({
-  path: path.join(__dirname, '../.env')
-});
-
 module.exports = {
     PORT: 3000,
     MONGO_CONNECTION_STRING: "mongodb+srv://user:user@cluster0.jijfxzy.mongodb.net/?retryWrites=true&w=majority"
