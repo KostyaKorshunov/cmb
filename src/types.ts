8b13e25f161ecfb12cdecf7b71929b48fbc84558
KostyaKorshunov <korshunovk@gmail.com>
2022-09-05 13:54:45 +0300

    35

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type typeUser = {
+    id: number,
+    name: string,
+    pass: string,
+    email: string,
+    lang: string,
+    currency: string,
+    avatar: string
+}
+
+export type typeDBUser = {
+    id: number,
+    name: string,
+    pass: string,
+    email: string,
+    lang: string,
+    curr: string,
+    avatar: string
+}
