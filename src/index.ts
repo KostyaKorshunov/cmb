788d357d73391fe3285490ca6c30c5e1b5dc3177
KostyaKorshunov <korshunovk@gmail.com>
2022-09-05 17:26:51 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { App } from './app';
 // const config = require( './config.json' );
 
 let app = new App();
-app.expressApp.listen(function() {  // 8080, "0.0.0.0", 
-    console.log(`App listening at port ${8080}`);
+app.expressApp.listen(process.env.PORT || 3000, function() {  // 8080, "0.0.0.0", 
+    console.log(`App listening at port ${3000}`);
 });
